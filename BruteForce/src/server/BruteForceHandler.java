9712bd0c1bdabe971f83ee094c5fc99dc6172697
Sean Syed <seansyed@usc.edu>
2018-11-02 11:45:23 -0700

    Beginning servlet work

--- BruteForce/src/server/BruteForceHandler.java
+++ BruteForce/src/server/BruteForceHandler.java
@@ -1,16 +1,14 @@
 package server;
 
-import com.google.gson.Gson;
-
 public class BruteForceHandler {
 	public BruteForceHandler() { }
 	public void handleRequest(String callType) {
 		switch (callType) {
-		case "add_user":
-			System.out.println("add_user");
+		case "addUser":
+			
 			break;
-		case "check_class_list":
-			System.out.print("check_class_list");
+		case "checkClassList":
+			
 			break;
 		default:
 			break;
