6baeabedffe5b4c745c61b26bca2fe00589cfde2
costyneuf <youzi_g@163.com>
2018-11-14 00:20:19 -0800

    update crawler for multithreading

--- BruteForce/src/algorithm/CourseCrawling.java
+++ BruteForce/src/algorithm/CourseCrawling.java
@@ -10,20 +10,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import database.DatabaseHandler;
-import model.BuildingCandidate;
-import model.Course;
-import model.Section; 
+import model.BuildingCandidate; 
 
 public class CourseCrawling {
 	
-	/* 
-	 *	private static final String TERM = "20183";
-	 *  private static final String BUILDING_URL = "https://web-app.usc.edu/maps/all_map_data2.js.pagespeed.jm.HVbGqVyZmP.js";
-	 *  CourseCrawling webCrawl = new CourseCrawling();
-	 *  webCrawl.start("https://classes.usc.edu/term-" + TERM, BUILDING_URL);
-	 */
-	
-	private static final int TIMEOUT = 2000000, SLEEPTIME = 10;
+	private static final int TIMEOUT = 2000000;
 	private List<String> courseLinks, names, schools;
 	private String currentSchool = "";
 	private DatabaseHandler dbh;
@@ -50,12 +41,25 @@ public class CourseCrawling {
 		
 		// Update course table
 		crawlTermPage(url);
+		SectionCrawler[] threads = new SectionCrawler[courseLinks.size()];
+		int i = 0;
 		while (!courseLinks.isEmpty()) {
 			String link = courseLinks.remove(0); // Course page link
 			String name = names.remove(0); // Major full name
 			String school = schools.remove(0);
-			System.out.println("\tStart processing: " + link + "\t" + name);		
-			crawlCoursePage(link, name, school);
+			threads[i] = new SectionCrawler(link, name, school, i);
+			i++;
+		}
+		for (i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+		
+		for (i = 0; i < threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		// Close DB connection
@@ -111,17 +115,20 @@ public class CourseCrawling {
 		
 	}
 	
-	private BufferedReader getReader(String link) throws IOException {
+	public static BufferedReader getReader(String link) throws IOException {
 		BufferedReader reader = null, tmp = null;
 		try {
 			URL url = new URL(link);
 			// Initialize a HTTP connection.
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			// Initialize a GET request.
-			connection.setRequestMethod("GET");
-			// Set up time out values.
-			connection.setConnectTimeout(TIMEOUT);
-			connection.setReadTimeout(TIMEOUT);
+			HttpURLConnection connection = null;
+			while (connection == null || connection.getResponseCode() != 200) {
+				connection = (HttpURLConnection) url.openConnection();
+				// Initialize a GET request.
+				connection.setRequestMethod("GET");
+				// Set up time out values.
+				connection.setConnectTimeout(TIMEOUT);
+				connection.setReadTimeout(TIMEOUT);
+			}
 			// If successfully connected, then get the data from the web page.
 			if (connection.getResponseCode() == 200) {
 				System.out.println("\tSuccessfully connected! " + url);
@@ -138,194 +145,8 @@ public class CourseCrawling {
 		return reader;
 	}
 
-	private void crawlCoursePage(String link, String name, String school) {
-		try {
-			BufferedReader reader = getReader(link);
-			if (reader != null) {
-				String line = "";     
-				while ((line = reader.readLine()) != null) {
-					if (isCourseDescription(line)) {
-						processCourseLine(line, school, name);
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		// TODO: Add multi-threading here.
-		try {
-			Thread.sleep(SLEEPTIME);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-	private void processCourseLine(String line, String school, String major) {
-
-		String startLocation = "<div class=\"course-info expandable\" id=\"";
-		String endLocation = "</td></tr></table></div></div>";
-		while (line.contains(startLocation)) {
-			int startIndex = line.indexOf(startLocation) + startLocation.length();
-			int endIndex = line.indexOf(endLocation) + endLocation.length();
-			// Trim the given {@line} and get the new line
-			String newLine = line.substring(startIndex, endIndex);		
-			line = line.substring(endIndex);
-					
-			// Update each course under given {@code major}
-			int courseID = addCourseToDB(newLine, school, major);
-			// Process section for each course 
-			addSectionToDB(newLine, major, courseID);
-			
-		}	
-	}
-
-	private void addSectionToDB(String line, String major, int courseID) {
-		if (courseID < 0) return;
-		System.out.println("\t\t\t\tRequesting data for course sections...");
-		String startLocation = "<tr data-section-id=", endLocation = "</tr>";
-		List<Section> sections = new ArrayList<>();
-		List<Section> lectures = new ArrayList<>();
-		// Assume all labs, discussions, and quizzes can be binded with all lectures.
-		List<String> lectureSection_IDs = new ArrayList<>();
-		while (line.contains(startLocation)) {
-			String tmp = line.substring(line.indexOf(startLocation));
-			line = tmp.substring(tmp.indexOf(endLocation));
-			String sectionID = getInnerHTMLByClassName(tmp, "section");
-			String type = getInnerHTMLByClassName(tmp, "type");
-			if (type.contains("Lecture")) type = "Lecture";
-			// Process time {[x]x:xx-[x]x:xx(a/p)m}
-			String[] times = processTime(getInnerHTMLByClassName(tmp, "time"));	
-			if (times != null) {
-				// Process days
-				String days = processDays(getInnerHTMLByClassName(tmp, "days").toUpperCase());
-				if (days != null) {
-					// Process instructor
-					String instructor = processInstructor(getInnerHTMLByClassName(tmp, "instructor"));
-					// Process class capacity
-					String classCapacityString = getInnerHTMLByClassName(tmp, "registered");
-					classCapacityString = classCapacityString.substring(classCapacityString.indexOf("of ") + 3);
-					classCapacityString = classCapacityString.substring(0, classCapacityString.indexOf("<"));
-					int classCapacity = classCapacityString.contains(":") || classCapacityString.contains(" ") ? 0 : Integer.parseInt(classCapacityString);
-					if (classCapacity > 0) {
-						String building_ID = tmp.contains("\"map\"") ? getInnerHTMLByClassName(tmp, "map").substring(0,3) : "TBA";
-						
-						Section section = new Section(sectionID, type, times[0], times[1], days,
-								instructor, building_ID, classCapacity, courseID);
-						// Add lecture id
-						if (section.isLecture()) {
-							lectures.add(section);
-							lectureSection_IDs.add(sectionID);
-						} else {
-							// Add a new section object
-							sections.add(section);
-						}
-					}
-				}
-			}
-		}
-		
-		// Write data into DB
-		System.out.println("\t\t\t\tWriting data into DB for course sections...");
-		for (int i = 0; i < lectures.size(); i++) {
-			dbh.addSection(lectures.get(i));
-		}
-		for (int i = 0; i < sections.size(); i++) {
-			Section section = sections.get(i);
-			if (!section.isLecture()) {
-				for (int j = 0; j < lectureSection_IDs.size(); j++)
-					section.setLectureSection_ID(lectureSection_IDs.get(j));
-			}
-			dbh.addSection(section);
-		}
-		
-	}
 	
-	private String processInstructor(String innerHTML) {
-		if (innerHTML.contains("<a href=")) {
-			// System.out.println(innerHTML);
-			innerHTML = innerHTML.substring(innerHTML.indexOf("\">") + 2, innerHTML.indexOf("</"));
-		}
-		if (innerHTML.contains(",")) innerHTML = innerHTML.substring(0, innerHTML.indexOf(","));
-		return innerHTML;
-	}
 
-	private String[] processTime(String time) {
-		
-		if (time.contains("TBA") || time.length() <= 0) return new String[] {"23:58", "23:59"};
-		
-		String start_time = time.substring(0, time.indexOf("-"));
-		String end_time = time.substring(time.indexOf("-") + 1, time.length() - 2);
-		if (time.substring(time.length() - 2).contains("p")) {
-			int start_h = Integer.parseInt(start_time.substring(0, start_time.indexOf(":")));
-			int end_h = Integer.parseInt(end_time.substring(0, end_time.indexOf(":")));
-			if ( start_h <= end_h && end_h != 12 ) {
-				start_time =  (start_h + 12) + start_time.substring(start_time.indexOf(":"));
-			}
-			if ( end_h != 12) {
-				end_time =  (end_h + 12) + end_time.substring(end_time.indexOf(":"));
-			}
-		}
-		return new String[] {start_time, end_time};
-	}
-
-	private String getInnerHTMLByClassName(String line, String className) {
-		// System.out.print("Processing " + className + ":\t");
-		String startLocation = "class=\"" + className + "\">";
-		line = line.substring(line.indexOf(startLocation));
-		line = line.substring(0, line.indexOf("</td"));
-		// System.out.println(line.substring(line.indexOf(">") + 1));
-		
-		return line.substring(line.indexOf(">") + 1);
-		
-	}
-
-	private String processDays(String days) {
-		if (days.contains("TBA")) return "MTWHF";
-		String result = "";
-		char[] symbols = {'M', 'T', 'W', 'H', 'F'};
-		for (int i = 0; i < symbols.length; i++) {
-			result += days.indexOf(symbols[i]) < 0 ? "" : symbols[i];
-		}
-		return result;
-	}
-
-	private int addCourseToDB(String line, String school, String major) {
-
-		Course course = new Course(school, major);
-		
-		/*
-		 * Update number, name, description, units
-		 */
-		
-		// Get number
-		int startIndex = line.indexOf("<strong>" + major + " ") 
-				+ (new String("<strong>" + major + " ")).length();
-		int endIndex = line.indexOf(":</strong>");
-		String number = line.substring(startIndex, endIndex);
-		course.setNumber(number);
-		// Get name
-		startIndex = line.indexOf("</strong>") + (new String("</strong>")).length();
-		endIndex = line.indexOf("<span class=\"uni");
-		String name = line.substring(startIndex, endIndex);
-		line = line.substring(endIndex + 6);
-		course.setName(name);
-		// Get units
-		float units = Float.parseFloat(line.substring(line.indexOf("(") + 1, line.indexOf("(") + 4));
-		course.setUnits(units);
-		// Get description
-		startIndex = line.indexOf("<div class=\"catalogue\">") + (new String("<div class=\"catalogue\">")).length();
-		line = line.substring(startIndex);
-		endIndex = line.indexOf("</div>");
-		String description = line.substring(0, endIndex);
-		course.setDescription(description);
-		
-		// Insert into database
-		dbh.addCourse(course);
-		
-		return dbh.getCourseId(course);
-	}
 
 	/**
 	 * 
@@ -393,9 +214,5 @@ public class CourseCrawling {
 		return false;
 	}
 	
-	private boolean isCourseDescription(String line) {
-		if (line.contains("<div class=\"course-info expandable\""))
-			return true;
-		return false;
-	}
+	
 }
